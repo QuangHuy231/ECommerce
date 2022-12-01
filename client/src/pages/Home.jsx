cfd59f0a717fdf84f27cdfcd00b7873bac4f2fcf
Quang Huy <nguyenquanghuya3kd@gmail.com>
2022-12-01 20:41:02 +0700

    use bootstrap and use components

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect } from "react";
-import { Link } from "react-router-dom";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import logger from "use-reducer-logger";
 import axios from "axios";
 import { useReducer } from "react";
+import Product from "../components/Product";
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -47,24 +49,13 @@ const Home = () => {
         ) : err ? (
           <div>{err}</div>
         ) : (
-          <div className="products">
+          <Row>
             {products.map((product) => (
-              <div key={product.slug} className="product">
-                <Link to={`/product/${product.slug}`}>
-                  <img src={product.image} alt={product.name} />
-                </Link>
-                <div className="product-info">
-                  <Link to={`/product/${product.slug}`}>
-                    <p>{product.name}</p>
-                  </Link>
-                  <p>
-                    <strong>{product.price}</strong>
-                  </p>
-                  <button>Add to Cart</button>
-                </div>
-              </div>
+              <Col key={product.slug} sm={6} md={4} lg={3} className="mb-3">
+                <Product product={product} />
+              </Col>
             ))}
-          </div>
+          </Row>
         )}
       </div>
     </>
