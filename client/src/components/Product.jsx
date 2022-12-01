6b387c4082ac612a49797ea48f24c07a89b22f4a
Quang Huy <nguyenquanghuya3kd@gmail.com>
2022-12-01 20:58:08 +0700

    add rating and number reviews

--- client/src/components/Product.jsx
+++ client/src/components/Product.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
+import Rating from "./Rating";
 const Product = (props) => {
   const { product } = props;
   return (
@@ -13,6 +14,7 @@ const Product = (props) => {
         <Link to={`/product/${product.slug}`}>
           <Card.Title>{product.name}</Card.Title>
         </Link>
+        <Rating rating={product.rating} numReviews={product.numReviews} />
         <Card.Text>${product.price}</Card.Text>
         <Button>Add to Cart</Button>
       </Card.Body>
