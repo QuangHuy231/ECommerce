dfda21ad9e660e90ae0fe5621c13318c3649b56c
Quang Huy <nguyenquanghuya3kd@gmail.com>
2022-12-05 09:14:33 +0700

    create Product screen

---
+++ README.md
@@ -0,0 +1,41 @@
+1.Create React App
+2.Create Git Repository
+3.List Products
+create products array
+add product images
+render products
+style products
+4.Add page routing
+npm i react-router-dom
+create route for home screen
+create router for product screen
+5.Create Node.JS Server
+run npm init in root folder
+Update package.json set type: module
+Add .js to imports
+npm install express
+create server.js
+add start command as node backend/server.js
+require express
+create route for / return backend is ready.
+move products.js from frontend to backend
+create route for /api/products
+return products
+run npm start
+6.Fetch Products From Backend
+set proxy in package.json
+npm install axios
+use state hook
+use effect hook
+use reducer hook
+7.Manage State By Reducer Hook
+define reducer
+update fetch data
+get state from usReducer
+8.Add bootstrap UI Framework
+npm install react-bootstrap bootstrap
+update App.js
+9.Create Product and Rating Component
+create Rating component
+Create Product component
+Use Rating component in Product component
--- client/src/pages/SinglePage.jsx
+++ client/src/pages/SinglePage.jsx
@@ -1,7 +1,113 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useReducer } from "react";
+import Col from "react-bootstrap/esm/Col";
+import Row from "react-bootstrap/Row";
+import ListGroup from "react-bootstrap/ListGroup";
+import Card from "react-bootstrap/Card";
+import Badge from "react-bootstrap/Badge";
+import { useParams } from "react-router-dom";
+import Rating from "../components/Rating";
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "FETCH_REQUEST":
+      return { ...state, loading: true };
+    case "FETCH_SUCCESS":
+      return { ...state, product: action.payload, loading: false };
+    case "FETCH_FAIL":
+      return { ...state, err: action.payload, loading: false };
+    default:
+      return state;
+  }
+};
 const SinglePage = () => {
-  return <>Single</>;
+  const params = useParams();
+  const { slug } = params;
+  const [{ loading, product, err }, dispath] = useReducer(reducer, {
+    product: [],
+    loading: false,
+    err: "",
+  });
+  // const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      dispath({ type: "FETCH_REQUEST" });
+      try {
+        const result = await axios.get(`/api/product/${slug}`);
+        dispath({ type: "FETCH_SUCCESS", payload: result.data });
+      } catch (error) {
+        dispath({ type: "FETCH_FAIL", payload: error.message });
+      }
+
+      // setProducts(result.data);
+    };
+    fetchProducts();
+  }, [slug]);
+
+  return (
+    <div>
+      {loading ? (
+        <div>Loading...</div>
+      ) : err ? (
+        <div>{err}</div>
+      ) : (
+        <div>
+          <Row>
+            <Col md={6}>
+              <img
+                src={product.image}
+                alt={product.name}
+                className="img-large"
+              />
+            </Col>
+            <Col md={3}>
+              <ListGroup variant="flush">
+                <ListGroup.Item>
+                  <h1>{product.name}</h1>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <Rating
+                    rating={product.rating}
+                    numReviews={product.numReviews}
+                  />
+                </ListGroup.Item>
+                <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
+                <ListGroup.Item>
+                  Description: <p>{product.description}</p>
+                </ListGroup.Item>
+              </ListGroup>
+            </Col>
+            <Col md={3}>
+              <Card>
+                <Card.Body>
+                  <ListGroup variant="flush">
+                    <ListGroup.Item>
+                      <Row>
+                        <Col>Price:</Col>
+                        <Col>${product.price}</Col>
+                      </Row>
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                      <Row>
+                        <Col>Status:</Col>
+                        <Col>
+                          {product.countInStock > 0 ? (
+                            <Badge bg="success">In Stock</Badge>
+                          ) : (
+                            <Badge bg="danger">Unavailable</Badge>
+                          )}
+                        </Col>
+                      </Row>
+                    </ListGroup.Item>
+                  </ListGroup>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default SinglePage;
