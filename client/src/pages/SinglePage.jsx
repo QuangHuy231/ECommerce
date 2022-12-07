91c8054c0a8333f5f203ac302f709e8a5eb17981
Quang Huy <nguyenquanghuya3kd@gmail.com>
2022-12-07 18:51:21 +0700

    improve cart item

--- client/src/pages/SinglePage.jsx
+++ client/src/pages/SinglePage.jsx
@@ -51,8 +51,16 @@ const SinglePage = () => {
   }, [slug]);
 
   const { state, dispath: ctxDispath } = useContext(Store);
-  const addToCartHandler = () => {
-    ctxDispath({ type: "CART_ADD_ITEM", payload: { ...product, quantity: 1 } });
+  const { cart } = state;
+  const addToCartHandler = async () => {
+    const existItem = cart.cartItems.find((x) => x._id === product._id);
+    const quantity = existItem ? existItem.quantity + 1 : 1;
+    const { data } = await axios.get(`/api/products/${product._id}`);
+    if (data.countInStock < quantity) {
+      window.alert("Sorry, Product is out of stock");
+      return;
+    }
+    ctxDispath({ type: "CART_ADD_ITEM", payload: { ...product, quantity } });
   };
   return (
     <div>
